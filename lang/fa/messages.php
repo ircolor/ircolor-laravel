2e3c663016c0e5a39af7df5f2c192f5d4a3da03f
Amir Tavakolian <rezatva@gmail.com>
2024-09-17 16:08:44 +0330

    add multi lang files

---
+++ lang/fa/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    "Something went wrong, please try again later" => "خطا در ثبت نام کاربر. لطفا مجددا تلاش کنید",
+    "User has been registered successfully" => "کاربر با موفقیت ثبت نام شد"
+];
