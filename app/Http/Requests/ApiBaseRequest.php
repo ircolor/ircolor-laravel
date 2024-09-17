2151196fd37d46e48daee17cef7ea8e100d802c3
Amir Tavakolian <rezatva@gmail.com>
2024-09-17 14:16:27 +0330

    add BaseRequest for all api form requests

---
+++ app/Http/Requests/ApiBaseRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ApiBaseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'ok' => false,
+            'message' => 'Validation Error',
+            'data' => $validator->errors()
+        ], 422));
+    }
+}
