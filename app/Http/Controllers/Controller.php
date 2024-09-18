4a0eac12713c3d55184b532131d524c4bdcbc5de
Amir Tavakolian <rezatva@gmail.com>
2024-09-18 16:41:51 +0330

    install l5-swagger and config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     title="IRColor API Documentation",
+ *     description="IRColor API Documentation",
+ *     version="1.0.0"
+ * ),
+ * @OA\Tag(
+ *     name="Authentication"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
