0482f0254f663b1a57db569506279ec260e2239d
Amir Tavakolian <rezatva@gmail.com>
2024-09-19 16:40:11 +0330

    refactor: change swagger doc path

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -12,7 +12,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                  */
-                'api' => 'api/ir-color/document',
+                'api' => 'api/documentation',
             ],
             'paths' => [
                 /*
