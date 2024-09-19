4f9318c915cb9d3c047a5e21f9b0f4c7ad264d49
Amir Tavakolian <rezatva@gmail.com>
2024-09-19 16:24:34 +0330

    refactor: remove useless authorize() methods

--- app/Http/Requests/Api/AuthLoginRequest.php
+++ app/Http/Requests/Api/AuthLoginRequest.php
@@ -9,14 +9,6 @@ use Illuminate\Support\Facades\Hash;
 class AuthLoginRequest extends ApiBaseRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, ValidationRule|array<mixed>|string>
--- app/Http/Requests/ApiBaseRequest.php
+++ app/Http/Requests/ApiBaseRequest.php
@@ -10,14 +10,6 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 class ApiBaseRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, ValidationRule|array<mixed>|string>
