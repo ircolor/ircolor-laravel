68aa5e8bb9eea0b345e08ff639b37b40788e9873
Amir Tavakolian <rezatva@gmail.com>
2024-09-19 15:15:40 +0330

    refactor: add / route to prevent test failure

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\Api\V1\GoogleController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::group(['middleware' => 'web'], function () {
     Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
     Route::get('redirect/google', [GoogleController::class, 'handleGoogleCallback']);
