2f7f6ab102933e0d678430cf6d78a8b74abf6d93
Amir Tavakolian <rezatva@gmail.com>
2024-09-23 13:38:56 +0330

    refactor: remove useless words in translation file

--- lang/fa/messages.php
+++ lang/fa/messages.php
@@ -3,8 +3,8 @@
 return [
     "registration_error" => "خطا در ثبت نام کاربر. لطفا مجددا تلاش کنید",
     "registered_successfully" => "کاربر با موفقیت ثبت نام شد",
-    "palette_created_successfully" => "پالت با موفقیت ساخته شد",
-    "palette_updated_successfully" => "پالت با موفقیت آپدیت شد",
-    "palette_deleted_successfully" => "پالت با موفقیت حذف شد",
+    "created_successfully" => "با موفقیت ساخته شد",
+    "updated_successfully" => "با موفقیت آپدیت شد",
+    "deleted_successfully" => "با موفقیت حذف شد",
     "unauthorized_access" => "دسترسی غیر مجاز"
 ];
