7ddb003267212c3090ac7ff2e3d2033900f009d8
Amir Tavakolian <rezatva@gmail.com>
2024-09-25 10:51:18 +0330

    add collections controller, model, migration

---
+++ app/Http/Controllers/Api/CollectionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Collection;
+use Illuminate\Http\Request;
+
+class CollectionController extends Controller
+{
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CollectionController;
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\PaletteController;
 use Illuminate\Support\Facades\Route;
@@ -9,10 +10,9 @@ Route::group(['prefix' => '/v1'], function () {
     Route::post('/auth/register', [AuthController::class, 'register']);
     Route::post('/auth/login', [AuthController::class, 'login']);
 
-    Route::apiResource('palettes', PaletteController::class)
-        ->only(['store', 'update', 'destroy'])
-        ->middleware('auth:sanctum');
-
+    Route::apiResource('palettes', PaletteController::class)->only(['store', 'update', 'destroy'])->middleware('auth:sanctum');
     Route::apiResource('palettes', PaletteController::class)->only(['index', 'show']);
+
+    Route::apiResource('collections', CollectionController::class)->middleware('auth:sanctum');
 
 });
