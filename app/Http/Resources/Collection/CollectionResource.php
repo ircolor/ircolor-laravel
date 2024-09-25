99b6ab4dd4f63e16a76bd29cbb5512863860f18d
Amir Tavakolian <rezatva@gmail.com>
2024-09-25 21:48:22 +0330

    chore: return collection id with collection resource

--- app/Http/Resources/Collection/CollectionResource.php
+++ app/Http/Resources/Collection/CollectionResource.php
@@ -16,6 +16,7 @@ class CollectionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'user' => new UserResource($this->user),
         ];
